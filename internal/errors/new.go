607651c88b076dfbfdcecb9dcc3e5082b6601230
Max Mei <maxmei@juzhou.io>
2020-10-22 17:09:07 +0800

    simplify datalake library

---
+++ internal/errors/new.go
@@ -0,0 +1,16 @@
+package errors
+
+import "fmt"
+
+type dataLakeError struct {
+	err error
+	msg string
+}
+
+func Wrap(err error, msg string) *dataLakeError{
+	return &dataLakeError{err: err, msg: msg}
+}
+
+func (e *dataLakeError) Error() string {
+	return fmt.Sprintf("%s; %s", e.err, e.msg)
+}
